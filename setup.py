48c4ea2c1430efa356073eba888300c5687f2983
Daniel Malik <mail@fronbasal.de>
2019-10-02 12:49:13 +0200

    feat: move packages to proper directory

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='unifipy',
     version='1.0.0',
@@ -8,5 +11,14 @@ setup(
     license='MIT License',
     author='Daniel Malik',
     author_email='mail@fronbasal.de',
-    description='Unifi Python API', install_requires=['requests']
+    description='Unifi Python API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
