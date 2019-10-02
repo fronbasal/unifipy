1f27359af14acdb1a2572958ea5f9cf8523bb54b
Daniel Malik <mail@fronbasal.de>
2019-10-02 12:39:32 +0200

    feat: version 1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unifi Python Voucher API
+
+This API contains a Unifi API wrapper specifically for creating vouchers programmatically.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='unifipy',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/fronbasal/unifipy',
+    license='MIT License',
+    author='Daniel Malik',
+    author_email='mail@fronbasal.de',
+    description='Unifi Python API', install_requires=['requests']
+)
